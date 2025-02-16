3a8f9bad9f04cb6648bee3c0744776f5c1bc8058
p6nj <breee@duck.com>
2025-02-16 18:41:58 -0500

    navigation back home from create screen

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/ui/create/CreateScreen.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/ui/create/CreateScreen.kt
@@ -3,12 +3,14 @@ package com.ferhatozcelik.jetpackcomposetemplate.ui.create
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.ferhatozcelik.jetpackcomposetemplate.navigation.Screen
 
 
 @Composable
@@ -24,7 +26,9 @@ fun CreateScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Bonjour")
-
+        Button(onClick = { navController.navigate(Screen.Main.route) }) {
+            Text(text = "Retourner à la maison")
+        }
     }
 
 }
